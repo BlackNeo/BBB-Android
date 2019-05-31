51beb4593aabd5f90007423ffffec4d191af443c
BlackNeo <webscribo@gmail.com>
2019-05-31 02:16:34 +0200

    1.0.0: Android test version beta

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'bibine-sports',
+		wp_ws_url : 'https://bibinesports.fr/wp-appkit-api/bibine-sports',
+		wp_url : 'https://bibinesports.fr',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'Bibine Sports',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'lBx3+sbL7X7-kL@!T=[we!bz(Lw_CP |L!H?bL?,wSWx(,eNz.)~k*VuY1@IMKhK',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
